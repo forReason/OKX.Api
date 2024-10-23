6a9c646d7441f55240c3b4d626cb0a3ff9dae4e5
julian <julian.bechtold@outlook.com>
2024-10-23 21:24:43 +0200

    workaround for Position subscribe

--- OKX.Api/Common/Enums/OkxInstrumentType.cs
+++ OKX.Api/Common/Enums/OkxInstrumentType.cs
@@ -1,8 +1,11 @@
-﻿namespace OKX.Api.Common;
+﻿using Newtonsoft.Json.Converters;
+
+namespace OKX.Api.Common;
 
 /// <summary>
 /// OKX Instrument Type
 /// </summary>
+[JsonConverter(typeof(UppercaseEnumConverter))]
 public enum OkxInstrumentType
 {
     /// <summary>
@@ -46,4 +49,43 @@ public enum OkxInstrumentType
     /// </summary>
     [Map("CONTRACTS")]
     Contracts = 6,
+}
+public class UppercaseEnumConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        if (value is Enum enumValue)
+        {
+            // Convert the enum value to uppercase
+            writer.WriteValue(enumValue.ToString().ToUpper());
+        }
+        else
+        {
+            writer.WriteValue(value?.ToString());
+        }
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        // Read the value as a string
+        var enumText = reader.Value?.ToString()?.ToUpper();
+        if (enumText == null)
+            return existingValue;
+
+        // Try to parse the uppercase string back to the corresponding enum
+        try
+        {
+            return Enum.Parse(objectType, enumText, ignoreCase: true);
+        }
+        catch (ArgumentException)
+        {
+            // Return the default value if parsing fails
+            return existingValue;
+        }
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType.IsEnum;
+    }
 }
